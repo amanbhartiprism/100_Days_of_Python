fc8647707c941e3de365344663a95d1db375d088
Aman Bharti <amanbhartiprism@gmail.com>
2026-02-05 01:22:59 +0530

    Day 1: Band Name Generator Complete

---
+++ Day_1/main.py
@@ -0,0 +1,38 @@
+# print("Hello World!")
+# print("Hello World!\nHello World!")
+# print("Hello" + "Angela")
+# input("What is Your Name?")
+# print("Hello " + input("What is Your Name?"))
+# print("Hello " + input("What is Your Name?") + " !")
+# print("What is Your Name?")
+# name = input("What is Your Name?")
+# print(name)
+# name = "Jack"
+# print(name)
+# name = "Angela"
+# print(name)
+# print(len(name))
+# print(len(input("What is Your Name?")))
+# this thing below doesn't work it asks the name twice
+# username = input("What is Your Name?")
+# length = len(input("What is Your Name?"))
+# print(username)
+# print(length)
+# so we tried this and this one works
+# username = input("What is Your Name?")
+# length = len(username)
+# print(username)
+# print(length)
+# interchanging test
+# glass1 = "Milk"
+# glass2 = "Juice"
+# temp_glass = glass1
+# glass1 = glass2
+# glass2 = temp_glass
+# print(glass1)
+# print(glass2)
+# first project (just a name)
+# print("Welcome to the Band name Generator")
+# Home_City = input("Which City You Grew Up In?\n")  # we used \n to show the cursor on the next line
+# Pet_Name = input("What is Favorite Pet?\n")
+# print("Your band name could be " + Home_City + " " + Pet_Name)
